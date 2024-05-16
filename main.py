b4d8febd337b4bc78a5ab011b3801f12605b25e3
him-a-n-shu <108982340+him-a-n-shu@users.noreply.github.com>
2024-05-16 11:27:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def wishMe():
     else:
         speak("Good Evening!")
 
-    speak("I am Harry Sir. Please tell me how may I help you")
+    speak("I am PAPPU Sir. Please tell me how may I help you")
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
